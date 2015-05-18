c977fe198fcc8de4bae2586b8c3197a50190d4bb
Rotha <khoeurn.rotha@gmail.com>
2015-05-18 16:19:02 +0700

    Update on Mini_Project.java

--- src/Project/Mini_Project.java
+++ src/Project/Mini_Project.java
@@ -1,5 +1,16 @@
 package Project;
 
 public class Mini_Project {
+	public static void main(String[] args) {
+		String str = "Hello World";
+	      String anotherString = "hello world";
+	      Object objStr = str;
 
+	      System.out.println(str);
+	      System.out.println(anotherString);
+	      System.out.println(objStr);
+	      System.out.println( str.compareTo(anotherString) );
+	      System.out.println( str.compareToIgnoreCase(anotherString) );
+	      System.out.println( str.compareTo(objStr.toString()));
+	}
 }
